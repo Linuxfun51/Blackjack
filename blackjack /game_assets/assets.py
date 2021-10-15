11e7d787a07b537d3c795c2501ed9f7506bb3b9e
Linuxfun1990 <egeresi.mihaly2@gmail.com>
2021-10-15 18:49:55 +0200

    update

---
+++ blackjack /game_assets/assets.py
@@ -0,0 +1,131 @@
+import random
+
+class PlayerBase:
+	name_list = ["Gwenevere Gil","Madalyn Lavender","Hazel Ami","Mary Beth"]
+
+	def __init__(self):
+		self._name = None
+		self._credits = 0
+		self._hand = []
+		self._in_game = True
+	def create(self):
+		self._credits = random.randint(100, 1000)
+		self._name = random.choice(self.name_list)
+
+	def draw_card(self, deck):
+		# check hand value
+		# if hand value < 16 draw new card
+		# elif hand value >= 21 pass
+		while self._in_game:
+			hand_value = self.count_hand()
+			if hand_value > 16:
+				self._in_game = False
+				print(f"{self._name} passes.....")
+
+			else:
+				new_card = deck.give_card()
+				self._hand.append(new_card)
+
+
+	def count_hand(self):
+		return sum([card.value for card in self._hand])
+
+	def show_hand(self):
+		print(self._hand)
+
+
+
+	def __str__(self):
+		return self._name
+	
+	
+	
+	
+		
+
+class HumanPlayer(PlayerBase):
+	pass
+
+
+class AiPlayer(PlayerBase):
+	pass
+
+
+class Deck:
+	def __init__(self):
+		self._cards = []
+
+
+	def give_card(self):
+		new_card = self._cards[-1]
+		self._cards.remove(new_card)
+		return new_card
+
+	@property
+	def cards(self):
+		return self._cards
+
+
+
+	def create(self):
+		self._cards.clear()
+
+		cards = [
+			["2", 2],
+			["3", 3],
+			["4", 4],
+			["5", 5],
+			["6", 6],
+			["7", 7],
+			["8", 8],
+			["9", 9],
+			["10", 10],
+			["King", 10],
+			["Queen", 10],
+			["Jack", 10],
+			["Ace", 11]
+		]
+
+		names = ["Heart", "Club", "Diamond", "Spade"]
+
+		for name in names:
+			for card in cards:
+				card_name = f"{name} {card[0]}"
+				value = card[1]
+
+				new_card = Card(card_name, value)
+			   
+
+				self._cards.append(new_card)
+
+		random.shuffle(self._cards)
+
+
+	def show(self):
+		print(self._cards)	
+
+
+
+
+
+class Card:
+	def __init__(self, name, value):
+		self.name = name
+		self.value = value
+
+	def __repr__(self):
+		return f"{self.name}"
+
+
+if __name__ == '__main__':
+	deck = Deck()
+	deck.create()
+
+	player1 = AiPlayer()
+	player1.create()
+
+	player1.draw_card(deck)
+
+	player1.draw_card(deck)
+
+	player1.show_hand()
